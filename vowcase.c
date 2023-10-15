8e3bc67d8c755ec80008d863b8fa118d5eb9488c
Debobrata Dutta <142308382+Debobrata-Dutta@users.noreply.github.com>
2023-10-15 10:47:56 +0530

    Create vowcase.c

---
+++ vowcase.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter character:");
+    scanf("%c",&a);
+    switch(a)
+    {
+        case 'a':
+            printf("It is Vowel");
+            break;
+        case 'A':
+            printf("It is Vowel");
+            break;
+        case 'e':
+            printf("It is Vowel");
+            break;
+        case 'E':
+            printf("It is Vowel");
+            break;
+        case 'i':
+            printf("It is Vowel");
+            break;
+        case 'I':
+            printf("It is Vowel");
+            break;
+        case 'o':
+            printf("It is Vowel");
+            break;
+        case 'O':
+            printf("It is Vowel");
+            break;
+        case 'u':
+            printf("It is Vowel");
+            break;
+        case 'U':
+            printf("It is Vowel");
+            break;
+        default:
+            printf("It is a Consonant");
+    } }
