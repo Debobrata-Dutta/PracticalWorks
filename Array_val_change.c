3b790402216d18786a8359fdac22d0dd7a7bc8aa
Debobrata Dutta <142308382+Debobrata-Dutta@users.noreply.github.com>
2023-11-04 23:24:54 +0530

    Create Array_val_change.c

---
+++ Array_val_change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ int main() {
+
+     int s;
+
+     printf("ENTER THE ARRAY SIZE");
+
+     scanf("%d", &s);
+
+     int array[s];
+
+     for (int i = 0; i < s; i++) {
+
+         scanf("%d", &array[i]);
+     }
+     for (int i = 0; i < s; i++) {
+         printf(" %d", array[i]);
+     }
+     printf("\nwhat number you want too change");
+     int p;
+     scanf("%d", &p);
+     printf("enter change number");
+
+     int k;
+     scanf("%d", &k);
+
+     for (int i = 0; i < s; i++) {
+
+         if (p == array[i])
+
+             array[i] = k;
+
+
+     printf("%d ", array[i]);
+ }}
